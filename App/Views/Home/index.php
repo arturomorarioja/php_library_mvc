89d5d5e6c7c2fbfed0e240a68784deed28aaa9f7
Arturo Mora-Rioja <arturomora@gmail.com>
2025-04-13 10:24:50 +0200

    Cover image added. CSS amended

--- App/Views/Home/index.php
+++ App/Views/Home/index.php
@@ -1,6 +1,6 @@
 <?php include dirname(__DIR__) . '/Base/header.php'; ?>
 <?php include dirname(__DIR__) . '/Base/nav.php'; ?>
-    <main>
+    <main id="home">
         <p>Welcome to library <?=$library ?>, in the future <?=$futureLibrary ?>.</p>
         <section>
             <header>
@@ -12,5 +12,6 @@
                 <?php endforeach; ?>
             </ul>
         </section>
+        <img src="<?=\App\Config::BASE_URL ?>img/library.jpg" alt="KEA Library">
     </main>
 <?php include dirname(__DIR__) . '/Base/footer.php'; ?>
